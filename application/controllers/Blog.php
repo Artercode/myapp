7ce1dc22c9269dfb045cd9d47485844a94bc537c
artercode <arter@gmail.com>
2020-02-18 00:30:02 +0700

    04. Default Controller

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -4,11 +4,28 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Blog extends CI_Controller
 {
-    public function index($nama, $goldarah, $alamat)
+    public function index()
     {
-        $data['nama'] = $nama;
-        $data['goldarah'] = $goldarah;
-        $data['alamat'] = $alamat;
+        $data['blogs'] = [
+            [
+                'title'     => 'Artikel Pertama',
+                'content'   => '<p>Lorem Ipsum has been the industrys standard dummy 
+                text ever since the 1500s, when an unknown printer took a galley of type 
+                and scrambled it to make a type specimen book.</p>'
+            ],
+            [
+                'title'     => 'Artikel Kedua',
+                'content'   => '<p>Lorem Ipsum has been the industrys standard dummy 
+                text ever since the 1500s, when an unknown printer took a galley of type 
+                and scrambled it to make a type specimen book.</p>'
+            ],
+            [
+                'title'     => 'Artikel Ketiga',
+                'content'   => '<p>Lorem Ipsum has been the industrys standard dummy 
+                text ever since the 1500s, when an unknown printer took a galley of type 
+                and scrambled it to make a type specimen book.</p>'
+            ],
+        ];
 
         $this->load->view('blog', $data);
     }
--- application/views/blog.php
+++ application/views/blog.php
@@ -1,3 +1,19 @@
-<h1>Selamat Datang <?php echo $nama; ?></h1> <br>
-Golongan Darah Anda <?php echo $goldarah; ?> <br>
-Anda tinggal di <?php echo $alamat; ?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Artikel Terbaru</h1>
+    <?php foreach ($blogs as $key => $blog) : ?>
+        <div class="blog">
+            <h2><?php echo $blog['title']; ?></h2>
+            <?php echo $blog['content']; ?>
+        </div>
+    <?php endforeach; ?>
+</body>
+
+</html>
