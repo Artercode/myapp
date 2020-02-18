c29271a538cd1a2c56a0d052468161ca3bdeb33b
artercode <arter@gmail.com>
2020-02-18 20:07:11 +0700

    20. Menggunakan Library Form Validation

--- application/views/form_add.php
+++ application/views/form_add.php
@@ -5,22 +5,26 @@
         <!-- Post Content Column -->
         <div class="col-lg-8">
             <h1>Tambah Artikel</h1>
+            <div class="alert alert-warning">
+                <?php echo validation_errors(); ?>
+            </div>
+
             <?php echo form_open_multipart(); ?>
             <div class="form-group">
                 <label for="">Judul</label>
-                <?php echo form_input('title', null, 'class="form-control"'); ?>
+                <?php echo form_input('title', set_value('title'), 'class="form-control"'); ?>
             </div>
             <div class="form-group">
                 <label for="">URL</label>
-                <?php echo form_input('url', null, 'class="form-control"'); ?>
+                <?php echo form_input('url', set_value('url'), 'class="form-control"'); ?>
             </div>
             <div class="form-group">
                 <label for="">Konten</label>
-                <?php echo form_textarea('content', null, 'class="form-control"'); ?>
+                <?php echo form_textarea('content', set_value('content'), 'class="form-control"'); ?>
             </div>
             <div class="form-group">
                 <label>Cover</label>
-                <?php echo form_upload('cover', null, 'class="form-control"'); ?>
+                <?php echo form_upload('cover', set_value('cover'), 'class="form-control"'); ?>
             </div>
 
             <button type="submit" class="btn btn-primary">Simpan Artikel</button>
