4dcf47570f993b9316c037b2b730d984f293eeb9
artercode <arter@gmail.com>
2020-02-18 09:35:20 +0700

    06. Select Where

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -4,14 +4,31 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Blog extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper('url');
+    }
+
+
     public function index()
     {
-        $this->load->database();
-        $query = $this->db->query("SELECT * FROM blog");
+        $query = $this->db->get("blog");
         $data['blogs'] = $query->result_array();
 
-
         $this->load->view('blog', $data);
+    }
+
+    public function detail($url)
+    {
+        // $query = $this->db->query('SELECT * FROM blog WHERE url = "' . $url . '"');
+        $this->db->where('url', $url);
+        $query = $this->db->get('blog');
+
+        $data['blog'] = $query->row_array();
+
+        $this->load->view('detail', $data);
     }
 }
 
