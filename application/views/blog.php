d4053516d694887b6918fc8e56389ab1f15a833c
artercode <arter@gmail.com>
2020-02-18 15:18:45 +0700

    12. Templating Bootstrap Bagian 1

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,31 +1,159 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog Post - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./assets/css/blog-post.css" rel="stylesheet">
+
 </head>
 
 <body>
-    <h1>Artikel Terbaru</h1>
-    <a href="<?php echo site_url('blog/add'); ?>">Tambah Artikel</a>
-    <form action="">
-        <input type="text" name='find'>
-        <button type="submit">
-            Cari
-        </button>
-    </form>
-    <?php foreach ($blogs as $key => $blog) : ?>
-        <div class="blog">
-            <h2><a href="<?php echo site_url('blog/detail/' . $blog['url']); ?>">
-                    <?php echo $blog['title']; ?>
-                </a>
-            </h2>
-            <a href="<?php echo site_url('blog/edit/' . $blog['id']); ?>">Edit</a>
-            <a href="<?php echo site_url('blog/delete/' . $blog['id']); ?>">Delete</a>
-            <?php echo $blog['content']; ?>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Start Bootstrap</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="navitem">
+                        <a class="nav-link" href="<?php echo site_url('blog/add'); ?>">+ Tambah Artikel</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    <?php endforeach; ?>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <!-- Post Content Column -->
+            <div class="col-lg-8">
+                <!-- Title -->
+                <h1 class="mt-4">Artikel Terbaru</h1>
+                <!-- Author -->
+                <p class="lead">
+                    by
+                    <a href="#">Start Bootstrap</a>
+                </p>
+                <hr>
+
+                <?php foreach ($blogs as $key => $blog) : ?>
+                    <div class="post-preview">
+                        <a href="post.html">
+                            <h2 class="post-title">
+                                <?php echo $blog['title']; ?>
+                            </h2>
+                        </a>
+                        <p class="post-meta">Posted on <?php echo $blog['date']; ?>
+                            <a href="<?php echo site_url('blog/edit/' . $blog['id']); ?>">Edit</a>
+                            <a href="<?php echo site_url('blog/delete/' . $blog['id']); ?>">Delete</a>
+                        </p>
+                        <?php echo $blog['content']; ?>
+                    </div>
+                    <hr>
+                <?php endforeach; ?>
+            </div>
+
+
+
+
+            <!-- Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+                <!-- Search Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header">Search</h5>
+                    <div class="card-body mx-auto">
+                        <form action="">
+                            <input type="text" name="find">
+                            <button type="submit">
+                                Cari
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+                <!-- Categories Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header">Categories</h5>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#">Web Design</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">HTML</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Freebies</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="list-unstyled mb-0">
+                                    <li>
+                                        <a href="#">JavaScript</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">CSS</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Tutorials</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Side Widget -->
+                <div class="card my-4">
+                    <h5 class="card-header">Side Widget</h5>
+                    <div class="card-body">
+                        You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+        </div>
+        <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="./assets/vendor/jquery/jquery.min.js"></script>
+    <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
